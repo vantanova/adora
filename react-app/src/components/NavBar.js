a5f9d93fcc0e172bf7d5b06b8e405279c07bd1cc
Antonio <anguilar@aol.com>
2021-03-01 21:07:57 -0800

    first changes

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,70 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+import { Menu } from "antd";
+import "./authStyling/Navbar.css";
+import "antd/dist/antd.css";
 
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
+      <Menu mode="horizontal" defaultSelectedKeys={["2"]}>
+        <Menu.Item key="0" style={{ border: "none" }}>
+          <NavLink
+            to="/"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "#453823" }}
+          >
+            <h1 style={{ color: "#453823", margin: "0" }}>Adora</h1>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="1" style={{ border: "none" }}>
+          <NavLink
+            to="/"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "#453823" }}
+          >
             Home
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
+        </Menu.Item>
+        <Menu.Item key="2" style={{ border: "none" }}>
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "#453823" }}
+          >
             Login
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
+        </Menu.Item>
+        <Menu.Item key="3" style={{ border: "none" }}>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "#453823" }}
+          >
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
+        </Menu.Item>
+        <Menu.Item key="4" style={{ border: "none" }}>
+          <NavLink
+            to="/users"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "#453823" }}
+          >
             Users
           </NavLink>
-        </li>
-        <li>
+        </Menu.Item>
+        <Menu.Item key="5" style={{ border: "none", float: "right" }}>
           <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
+        </Menu.Item>
+      </Menu>
     </nav>
   );
-}
+};
 
 export default NavBar;
--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { logout } from "../../services/auth";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
-const LogoutButton = ({setAuthenticated}) => {
+const LogoutButton = ({ setAuthenticated }) => {
   const onLogout = async (e) => {
     await logout();
     setAuthenticated(false);
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return (
+    <Button
+      type="primary"
+      onClick={onLogout}
+      style={{ background: "#806854", borderColor: "#8d725c" }}
+    >
+      Logout
+    </Button>
+  );
 };
 
 export default LogoutButton;

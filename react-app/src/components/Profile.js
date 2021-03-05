f679ca61c0978fa934cacb33a2ecde76a6a87e98
Antonio <anguilar@aol.com>
2021-03-04 16:56:45 -0800

    many changes

--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import FlipPage from "react-flip-page";
-import "./Styling/Stickerbook.css";
+import { useSelector } from "react-redux";
+import { Card } from "antd";
+import "./Styling/Profile.css";
 
 function Profile() {
+  const sessionUser = useSelector((state) => state.session.user);
+  console.log(sessionUser);
   return (
-    <div>
-      <p>HI</p>
-    </div>
+    <Card className="main_profile_content">
+      <h1>HI</h1>
+    </Card>
   );
 }
 

576e1fdb369233085d2c4741a569ec1556c6c78b
Antonio <anguilar@aol.com>
2021-03-04 17:03:44 -0800

    many changes

--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Card } from "antd";
+import { Card, Row, Col } from "antd";
 import "./Styling/Profile.css";
+
+const { Meta } = Card;
 
 function Profile() {
   const sessionUser = useSelector((state) => state.session.user);
   console.log(sessionUser);
   return (
     <Card className="main_profile_content">
-      <h1>HI</h1>
+      <Row>
+        <Col span={8}>
+          <Card
+            className="profile_card"
+            cover={<img alt="example" src={sessionUser.photoUrl} />}
+          >
+            <Meta title={sessionUser.username} />
+          </Card>
+        </Col>
+        <Col>
+          <h1>About me</h1>
+        </Col>
+      </Row>
     </Card>
   );
 }

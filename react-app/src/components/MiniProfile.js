f3a4f30ac55c54f04de0039c606898ef9a784a86
Antonio <anguilar@aol.com>
2021-03-06 23:34:44 -0800

    styling profile pages

--- react-app/src/components/MiniProfile.js
+++ react-app/src/components/MiniProfile.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Card, Row, Col, Input, Tooltip, Button } from "antd";
+import { Card, Row, Col, Input, Tooltip, Button, Tabs } from "antd";
 import "./Styling/MiniProfile.css";
 import ProfileStickerbook from "./ProfileStickerbook";
 import { BookTwoTone, LikeTwoTone } from "@ant-design/icons";
@@ -8,36 +8,24 @@ import Avatar from "antd/lib/avatar/avatar";
 
 const { Meta } = Card;
 const { TextArea } = Input;
+const { TabPane } = Tabs;
 
 function MiniProfile(post) {
   console.log(post.profile);
   const userProfile = post.profile;
 
-  const [visibleBook, setVisibleBook] = useState(
-    <ProfileStickerbook
-      stickers={userProfile.owner.stickers}
-    ></ProfileStickerbook>
-  );
-
-  function setBook() {
-    if (visibleBook == null) {
-      setVisibleBook(
-        <ProfileStickerbook
-          stickers={userProfile.owner.stickers}
-        ></ProfileStickerbook>
-      );
-    } else {
-      setVisibleBook(null);
-    }
+  function callback(key) {
+    console.log(key);
   }
 
   return (
     post && (
       <Card className="post_profile_content">
         <Row>
-          <Col span={8}>
+          <Col span={5} className="mini_profile_card">
             <Card
-              className="mini_profile_card"
+              bordered={false}
+              height={"5vh"}
               cover={
                 // <img
                 //   className="mini_profile_photo"
@@ -45,58 +33,40 @@ function MiniProfile(post) {
                 //   src={userProfile.owner.photoUrl}
                 // />
                 <Avatar
-                  shape="square"
                   src={userProfile.owner.photoUrl}
                   className="mini_profile_photo"
                 />
               }
-            >
-              <Meta title={<h3>{userProfile.owner.username}</h3>} />
-            </Card>
+            ></Card>
             {/* <p className="status">{userProfile.owner.status}</p> */}
           </Col>
           <Col className="about">
             <h3>About {userProfile.owner.username}</h3>
             <p>{userProfile.owner.bio}</p>
-            <div className="profile_flex">
-              <Tooltip
-                title={`${userProfile.owner.username}'s Stickers`}
-                placement="bottom"
-              >
-                <Button type="text" style={{ height: "10vh" }}>
-                  <BookTwoTone
-                    twoToneColor="rgb(128, 104, 84)"
-                    style={{
-                      fontSize: "6vh",
-                      margin: "none",
-                      boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                    }}
-                    onClick={setBook}
-                  />
-                </Button>
-              </Tooltip>
-              <Tooltip
-                title={`${userProfile.owner.username}'s Likes`}
-                placement="bottom"
-              >
-                <Button type="text" style={{ height: "10vh" }}>
-                  <LikeTwoTone
-                    // twoToneColor="#eb2f96"
-                    style={{
-                      fontSize: "6vh",
-                      margin: "none",
-                      boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                    }}
-                    onClick={setBook}
-                  />
-                </Button>
-              </Tooltip>
-            </div>
+            <div className="profile_flex"></div>
           </Col>
         </Row>
         <Row>
           <Col span={24} style={{ width: "100%", height: "47.5vh" }}>
-            {visibleBook}
+            <Tabs defaultActiveKey="1" onChange={callback} type="card">
+              <TabPane
+                type="card"
+                tab={`${userProfile.owner.username}'s Stickers`}
+                key="1"
+              >
+                <div className="stickerbook_space">
+                  <ProfileStickerbook
+                    stickers={userProfile.owner.stickers}
+                  ></ProfileStickerbook>
+                </div>
+              </TabPane>
+              <TabPane tab={`${userProfile.owner.username}'s Likes`} key="2">
+                Content of Tab Pane 2
+              </TabPane>
+              <TabPane tab={`${userProfile.owner.username}'s Stamps`} key="3">
+                Content of Tab Pane 3
+              </TabPane>
+            </Tabs>
           </Col>
         </Row>
       </Card>
--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Card, Row, Col, Input, Tooltip, Button } from "antd";
+import { Card, Row, Col, Input, Tooltip, Button, Avatar } from "antd";
 import "./Styling/Profile.css";
 // import Stickerbook from "./Stickerbook";
 import "./Styling/MiniProfile.css";
-import ProfileStickerbook from "./ProfileStickerbook";
+import Stickerbook from "./Stickerbook";
 import { BookTwoTone, LikeTwoTone } from "@ant-design/icons";
-import Avatar from "antd/lib/avatar/avatar";
 
 const { Meta } = Card;
 const { TextArea } = Input;
@@ -14,16 +13,12 @@ const { TextArea } = Input;
 function Profile() {
   const sessionUser = useSelector((state) => state.session.user);
   const [visibleBook, setVisibleBook] = useState(
-    <ProfileStickerbook stickers={sessionUser.stickers}></ProfileStickerbook>
+    <Stickerbook stickers={sessionUser.stickers}></Stickerbook>
   );
 
   function setBook() {
     if (visibleBook == null) {
-      setVisibleBook(
-        <ProfileStickerbook
-          stickers={sessionUser.stickers}
-        ></ProfileStickerbook>
-      );
+      setVisibleBook(<Stickerbook></Stickerbook>);
     } else {
       setVisibleBook(null);
     }
@@ -32,13 +27,19 @@ function Profile() {
   return (
     sessionUser && (
       <Card className="main_profile_content">
+        <h1>My Profile</h1>
+        <hr></hr>
         <Row>
           <Col span={8}>
             <Card
               className="profile_card"
-              cover={<img alt="example" src={sessionUser.photoUrl} />}
+              bordered={false}
+              cover={
+                <Avatar src={sessionUser.photoUrl} className="profile_photo" />
+                // <img alt="example" src={sessionUser.photoUrl} />
+              }
             >
-              <Meta title={<h3>{sessionUser.username}</h3>} />
+              <Meta title={<Button>Upload a new photo</Button>} />
             </Card>
           </Col>
           <Col className="about">
@@ -49,16 +50,8 @@ function Profile() {
               maxLength="500"
               className="textarea"
             ></TextArea>
-            {/* <p>{sessionUser.bio}</p> */}
-          </Col>
-          <Col className="about">
-            <h3>About {sessionUser.username}</h3>
-            <p>{sessionUser.bio}</p>
-            <div className="profile_flex">
-              <Tooltip
-                title={`${sessionUser.username}'s Stickers`}
-                placement="bottom"
-              >
+            <div className="profile_page_flex">
+              <Tooltip title={`My Stickers`} placement="bottom">
                 <Button type="text" style={{ height: "10vh" }}>
                   <BookTwoTone
                     twoToneColor="rgb(128, 104, 84)"
@@ -71,10 +64,7 @@ function Profile() {
                   />
                 </Button>
               </Tooltip>
-              <Tooltip
-                title={`${sessionUser.username}'s Likes`}
-                placement="bottom"
-              >
+              <Tooltip title={`My Likes`} placement="bottom">
                 <Button type="text" style={{ height: "10vh" }}>
                   <LikeTwoTone
                     // twoToneColor="#eb2f96"
